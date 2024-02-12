503ce8636b021961b598a1f1da083be38939b2b7
Ivailo G <ivailogramatikov05@gmail.com>
2024-02-12 22:40:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "=3.0.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "StorageRGIvayloGramatikov"
+    storage_account_name = "taskboardstorage"
+    container_name = "taskboardcontainer"
+    key = "terraform.tfstate"
+  }
 }
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
